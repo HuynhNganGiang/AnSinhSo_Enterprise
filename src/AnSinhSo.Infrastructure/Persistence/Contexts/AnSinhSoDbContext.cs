b3bc3b1715b6d55a36ceeb742be03053d9b5ba79
HuynhNganGiang <sonhuynh2014.bt@gmail.com>
2026-07-24 11:59:46 +0700

    feat(persistence): add aggregate root dbsets

--- src/AnSinhSo.Infrastructure/Persistence/Contexts/AnSinhSoDbContext.cs
+++ src/AnSinhSo.Infrastructure/Persistence/Contexts/AnSinhSoDbContext.cs
@@ -1,9 +1,20 @@
+using AnSinhSo.Domain.Aggregates.CitizenAggregate;
+using AnSinhSo.Domain.Aggregates.HouseholdAggregate;
+using AnSinhSo.Domain.Aggregates.PaymentAggregate;
+using AnSinhSo.Domain.Aggregates.PolicyAggregate;
+using AnSinhSo.Domain.Aggregates.WelfareGroupAggregate;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnSinhSo.Infrastructure.Persistence.Contexts;
 
 public class AnSinhSoDbContext : DbContext
 {
+    public DbSet<Citizen> Citizens { get; set; }
+    public DbSet<Household> Households { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Policy> Policies { get; set; }
+    public DbSet<WelfareGroup> WelfareGroups { get; set; }
+
     public AnSinhSoDbContext(DbContextOptions<AnSinhSoDbContext> options) : base(options)
     {
     }
