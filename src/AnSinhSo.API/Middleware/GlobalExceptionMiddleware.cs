ef33b9fc09a13d2396cc66a77d35dbfc57e52d22
HuynhNganGiang <sonhuynh2014.bt@gmail.com>
2026-07-24 08:47:12 +0700

    feat(api): add global exception middleware

---
+++ src/AnSinhSo.API/Middleware/GlobalExceptionMiddleware.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AnSinhSo.Api.Middleware;
+
+public class GlobalExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        var statusCode = StatusCodes.Status500InternalServerError;
+        var message = "An error occurred while processing your request.";
+
+        switch (exception)
+        {
+            case UnauthorizedAccessException _:
+                statusCode = StatusCodes.Status401Unauthorized;
+                message = "Unauthorized access.";
+                break;
+            case KeyNotFoundException _:
+                statusCode = StatusCodes.Status404NotFound;
+                message = "Resource not found.";
+                break;
+            case Exception _:
+                statusCode = StatusCodes.Status500InternalServerError;
+                message = exception.Message;
+                break;
+        }
+
+        context.Response.StatusCode = statusCode;
+
+        var result = new
+        {
+            success = false,
+            message = message,
+            statusCode = statusCode
+        };
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        await context.Response.WriteAsync(JsonSerializer.Serialize(result, options));
+    }
+}
